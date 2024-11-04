371b44c8155a67ad6b29b02c8d87f3d6e25e76c8
poorvika6 <poorvika.ec23@bitsathy.ac.in>
2024-11-04 18:57:46 +0530

    landing page

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var pop=document.querySelector(".popup")
+var over=document.querySelector(".overlay")
+function showAlert() {
+  pop.style.display="block"
+  over.style.display="block"
+}
+function closeup()
+{
+    pop.style.display="none"
+    over.style.display="none"
+}
